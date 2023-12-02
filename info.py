b8e02aeddac306d1f668f6613af428c262234e9b
chaudhary662 <151464998+chaudhary662@users.noreply.github.com>
2023-12-02 22:09:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '23709433'))
-API_HASH = environ.get('API_HASH', '862baac6f1ba7b3c090ca5f86ef55f7c')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6883281957:AAFwA7R7ULuVnbSvZhy4hFUYav0FcKNOepM')
+API_ID = int(environ.get('23709433', ''))
+API_HASH = environ.get('862baac6f1ba7b3c090ca5f86ef55f7c', '')
+BOT_TOKEN = environ.get('6883281957:AAFwA7R7ULuVnbSvZhy4hFUYav0FcKNOepM', '')
 PORT = environ.get("PORT", "8080")
 
 # Bot settings
@@ -36,9 +36,9 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/72dff2b65352ba85d
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2a888a370f479f4338f7c.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5898561883').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('5898561883', '').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('-1001955187526', '0').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('auth_users', '5898561883').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('5898561883', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
@@ -51,15 +51,15 @@ SUPPORT_CHAT_ID = -1001792675255
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('mongodb+srv://mmm:Mn12345@cluster0.vdcqbt4.mongodb.net/?retryWrites=true&w=majority', "")
-DATABASE_NAME = environ.get('Cluster0', "PIRO")
+DATABASE_URI = environ.get('mongodb+srv://tatop53533:Nm123456@manish.wnnuk1j.mongodb.net/?retryWrites=true&w=majority', "")
+DATABASE_NAME = environ.get('Manish', "PIRO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
 # Others
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('-1002034507638', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
-LOG_CHANNEL = int(environ.get('-1002023629819', 0))
+LOG_CHANNEL = int(environ.get('-1002092271327', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'raixchat')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
@@ -73,7 +73,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('-1002032433259', '')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
